8abcfadafed492fbee7f27dce27f8a5def4b2b85
Adith25 <aditya98@mhs.usk.ac.id>
2026-03-17 10:49:48 +0700

    prepare backend deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { CertificateModule } from './certificate/certificate.module';
-import { ProjectModule } from './project/project.module';
-import { ExperienceModule } from './experience/experience.module';
-import { SkillModule } from './skill/skill.module';
-import { AboutCardModule } from './about-card/about-card.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { SettingModule } from './setting/setting.module';
-import { CvModule } from './cv/cv.module';
+import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
-    UserModule,
-    AuthModule,
-    CertificateModule,
-    ProjectModule,
-    ExperienceModule,
-    SkillModule,
-    AboutCardModule,
-    PrismaModule,
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'uploads'),
-    }),
-    SettingModule,
-    CvModule,
+      serveRoot: '/uploads',
+    })
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

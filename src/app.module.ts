1ffa1f90311a9a971797122b762bc598ee4fcd6a
Adith25 <aditya98@mhs.usk.ac.id>
2026-03-17 11:19:57 +0700

    fix missing module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,34 @@ import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 
+// IMPORT SEMUA MODULE
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { ProjectModule } from './project/project.module'
+import { ExperienceModule } from './experience/experience.module'
+import { CertificateModule } from './certificate/certificate.module'
+import { SkillModule } from './skill/skill.module'
+import { CvModule } from './cv/cv.module'
+import { AboutCardModule } from './about-card/about-card.module'
+import { SettingModule } from './setting/setting.module'
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
-    })
+    }),
+
+    // ⬇️ INI YANG PENTING
+    AuthModule,
+    UserModule,
+    ProjectModule,
+    ExperienceModule,
+    CertificateModule,
+    SkillModule,
+    CvModule,
+    AboutCardModule,
+    SettingModule,
   ],
 })
 export class AppModule {}

24c27cf57e3c68afcec0b58522c4097a0933beec
Adith25 <adityayufnanda25@gmail.com>
2026-05-12 00:35:23 +0700

    docs: add documentation for root service in app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  /**
+   * Returns a basic greeting string
+   */
   getHello(): string {
     return 'Hello World!';
   }
